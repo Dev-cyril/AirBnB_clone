c17b9b867118a2a5dbaa9f3571c77e1fed359488
Cyril <cyril.idu2@gmail.com>
2022-09-03 11:39:22 +0100

    updates

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""A module that inherits from the BaseModel class"""
+
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+  """A class that stores amenities to be accessed by the user"""
+  name = str()
